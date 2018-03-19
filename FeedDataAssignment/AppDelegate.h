d2ec2850a2771e01ca275a068ce94252db867daa
Richard Joseph <richard@Richards-MacBook-Air.local>
2018-03-19 23:36:27 +0530

    Initial Commit

---
+++ FeedDataAssignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FeedDataAssignment
+//
+//  Created by Richard Joseph on 19/03/18.
+//  Copyright © 2018 Richard Joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FeedDataAssignment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FeedDataAssignment
+//
+//  Created by Richard Joseph on 19/03/18.
+//  Copyright © 2018 Richard Joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
